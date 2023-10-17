022ad9cb5e7e799c6a5f8b6aee5816b5936e0424
rakelhskvikne <45000240+rakelhskvikne@users.noreply.github.com>
2023-10-17 20:54:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ df['toughness'] = pd.to_numeric(df['toughness'], errors='coerce')
 # Drop rows with missing values in power and toughness columns
 df = df.dropna(subset=['power', 'toughness'])
 
-# Select the features for clustering (e.g., 'power', 'toughness', and 'cmc')
+# Select the features for K-means clustering
 features = df[['power', 'toughness', 'cmc']]
 
 # Apply the elbow method to choose an appropriate value for k
@@ -37,16 +37,36 @@ plt.ylabel('Distortion')
 plt.title('Elbow Method for K-means Clustering')
 plt.show()
 
-# Fit and plot the K-means clustering results with k=2 in another separate figure
-kmeans = KMeans(n_clusters=2, random_state=42, n_init=10)
+# Fit and plot the K-means clustering results with k=3 in another separate figure
+kmeans = KMeans(n_clusters=3, random_state=42, n_init=10)
 df['cluster'] = kmeans.fit_predict(df[['power', 'toughness', 'cmc']])
 
 plt.figure(figsize=(8, 6))
 plt.scatter(df['power'], df['toughness'], c=df['cluster'], cmap='viridis')
 plt.xlabel('Power')
 plt.ylabel('Toughness')
-plt.title('K-means Clustering (k=2)')
+plt.title('K-means Clustering (k=3)')
 plt.show()
 
 plt.interactive(False)
 plt.show(block=True)
+
+
+# Select the features for EM clustering
+X = df[['power', 'toughness', 'cmc']]
+
+# Apply the Expectation-Maximization (EM) algorithm with k=3 clusters
+n_clusters = 3
+em = GaussianMixture(n_components=n_clusters, random_state=42)
+cluster_em = em.fit_predict(X)  # Store the cluster labels
+
+# Add the cluster labels to the dataframe
+df['cluster_em'] = cluster_em
+
+# Plot the clustering results
+plt.figure(figsize=(8, 6))
+plt.scatter(df['power'], df['toughness'], c=df['cluster_em'], cmap='viridis')
+plt.xlabel('Power')
+plt.ylabel('Toughness')
+plt.title(f'EM Clustering (k={n_clusters})')
+plt.show()
